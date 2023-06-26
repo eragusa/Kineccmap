fixed a number of issues and implemented new filtering for derivatives in all functions.

genvecc.py
@@ -41,7 +41,7 @@ M=up.M
 #nchannels=20
 
 #Npol for interpolation
-npol=20
+npol=up.npol
 
 def Omega0(a):
     return np.sqrt(G*M/a**3)
@@ -139,7 +139,7 @@ def generate_velocity_map(x,y,eccinp,phaseinp,sigmainp,Mainp,radprofinp,nprocs=1
     #Note that we fit with chebyshev also to smooth out the functions
     #Simple interpolation would be too rough to be used in the equation solver
     #for passing from xy-->a,phi
-    ee=itp.interpolator(radprof,ecc,2*npol)
+    ee=itp.interpolator(radprof,ecc,npol*2)
     def e(a):
         return ee(a)*(a<radprof[-int(frgrid*len(radprof))])
 
@@ -172,12 +172,12 @@ def generate_velocity_map(x,y,eccinp,phaseinp,sigmainp,Mainp,radprofinp,nprocs=1
     #to avoid weird peaks take derivative of complex phase
     #then divide by same and take imaginary part
     dvarpida=np.imag(np.gradient(np.exp(1.j*vp),radprof)/np.exp(1.j*vp))
-    dvpvpda=itp.interpolator(radprof,dvarpida,int(npol/2))
+    dvpvpda=itp.interpolator(radprof,dvarpida,2*npol)
     def dvpda_f(a):
         return dvpvpda(a)*(a<radprof[-int(frgrid*len(radprof))])
 
 
-    rho=sigma#/Hcirc(radprof)
+    rho=Ma_a(radprof)/6.28/radprof#/Hcirc(radprof) #to be understood why Ma makes such a big difference despite it's quite the same
     cs2rho=cs(radprof)**2*rho
     dPda1rhoa=np.nan_to_num(\
                      np.divide(\

interpolation.py
@@ -7,7 +7,10 @@ from scipy.signal import savgol_filter
 def interpolator(x,y,opt):
     option=opt #in case other variables need to be specified from the line
     #first filter with
-    y_filtered=savgol_filter(y,window_length=int(len(x)/7),polyorder=2)
+
+    #sistema come option che per sigma serve window stretta perche funzione e' molto steep
+
+    y_filtered=savgol_filter(y,window_length=int(len(x)/option),polyorder=2)
     #then interpolate
     interpolation_method = 'cubic'      
     interp = interp1d(x, y_filtered, kind=interpolation_method)

morphology.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pdb
 import discEccanalysis_pysplash as de
+import interpolation as itp
+import useful_param as up
 
-npol=20
+npol=up.npol
 
 def Jacobian_det(a,phi,e_f,sinvarpi_f,cosvarpi_f,deda_f,dvpda_f):
 
@@ -39,15 +41,15 @@ if __name__=='__main__':
     a=3.*np.ones(100)
     phi=np.linspace(0,np.pi*2.,100)
 
-    ee=np.polynomial.Chebyshev.fit(radprof,ecc,npol)
+    ee=itp.interpolator(radprof,ecc,npol)
     def e(a):
         return ee(a)*(a<radprof[-int(frgrid*len(radprof))])
 
-    sin_interp=np.polynomial.Chebyshev.fit(radprof,np.sin(varpi),npol*2)
+    sin_interp=itp.interpolator(radprof,np.sin(varpi),npol*2)
     def sinvarpi(a):
         return sin_interp(a)*(a<radprof[-int(frgrid*len(radprof))])
 
-    cos_interp=np.polynomial.Chebyshev.fit(radprof,np.cos(varpi),npol*2)
+    cos_interp=itp.interpolator(radprof,np.cos(varpi),npol*2)
     def cosvarpi(a):
         return cos_interp(a)*(a<radprof[-int(frgrid*len(radprof))])
 
@@ -58,11 +60,11 @@ if __name__=='__main__':
     #then divide by same and take imaginary part
     dvarpida=np.imag(np.gradient(np.exp(1.j*vp),radprof)/np.exp(1.j*vp))
 
-    deeda=np.polynomial.Chebyshev.fit(radprof,deda,npol)
+    deeda=itp.interpolator(radprof,deda,6*npol)
     def deda_f(a):
         return deeda(a)*(a<radprof[-int(frgrid*len(radprof))])
 
-    dvpvpda=np.polynomial.Chebyshev.fit(radprof,dvarpida,2*npol)
+    dvpvpda=itp.interpolator(radprof,dvarpida,2*npol)
     def dvpda(a):
         return dvpvpda(a)*(a<radprof[-int(frgrid*len(radprof))])
 

useful_param.py
@@ -1,8 +1,11 @@
 import numpy as np
 
+folderres='./analysis_paper/'
+#folderres='./sim5A2000/'
+img='png'
 #size of ellipses
 ain=2. #at the moment used as minimum radius beyond which strarting looking for the maximum density
-fracmax=0.2 #which percentage of density is used to define cavity edge
+fracmax=0.3 #which percentage of density is used to define cavity edge
 aout=10.
 a0=2.
 #eccentricity parameters
@@ -29,3 +32,5 @@ i0=0*50./180.*np.pi
 PA0=10./180.*np.pi
 #for channel maps
 nchannels=20
+####interpolation
+npol=3

velofield2.py
@@ -1,16 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import os
 import sys
 import discEccanalysis_pysplash as de
 import genvecc as gv
 import morphology as morph
+import useful_param as up
 #import vertical_structure as vs
 
-img='png'
+img=up.img
 #folderres='./analysis_paper/azimuth'
 #folderres='./figures_for_poster/azimuth'
-folderres='./sim5A2000/azimuth'
+folderres=up.folderres+'azimuth'
 
 name=sys.argv[1]
 
@@ -256,12 +258,27 @@ plt.ylabel('$y$')
 plt.axis('equal')
 plt.savefig(folderres+'/vyteor.'+img,dpi=400)
 
+velmax=v1v[1,:].max()*0.9
+velmin=-velmax
+nlevel=20
+lev=np.linspace(velmin,velmax,nlevel)
+plt.figure(81)
+plt.scatter(x1v[0,:],x1v[1,:],c=vy_press,cmap="RdBu_r",vmin=velmin,vmax=velmax)
+plt.colorbar()
+plt.tricontour(x1v[0,:],x1v[1,:],vy_press,levels=lev, linewidths=0.5, colors='k')
+plt.title('$v_{y,teor}$')
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.axis('equal')
+plt.savefig(folderres+'/vyteor_press.'+img,dpi=400)
+
+
 plt.figure(4)
 plt.scatter(xgrplan,ygrplan,c=vyplan,cmap="RdBu_r",vmin=velmin,vmax=velmax)
 #plt.pcolormesh(x,y,vy,cmap="RdBu_r",vmin=velmin,vmax=velmax)
 plt.colorbar()
 #plt.contour(x,y,vy,levels=lev, linewidths=0.5, colors='k')
-plt.tricontour(xgrplan,ygrplan,vyplan,levels=20, linewidths=0.5, colors='k')
+plt.tricontour(xgrplan,ygrplan,vyplan,levels=lev, linewidths=0.5, colors='k')
 plt.title('$v_{y,sim}$')
 plt.xlabel('$x$')
 plt.ylabel('$y$')
@@ -269,8 +286,8 @@ plt.axis('equal')
 plt.savefig(folderres+'/vysim.'+img,dpi=400)
 
 plt.figure(51)
-vmax=np.max(Ma/6.28)
-vmin=np.min(Ma/6.28)
+vmax=np.max(Ma/6.28)/2.5
+vmin=np.max(Ma/6.28)
 plt.scatter(xgrplan,ygrplan,c=SigmaEcc,vmin=vmin,vmax=vmax,cmap="inferno")
 plt.colorbar()
 plt.title('$\Sigma_{\\rm teor}(a,\phi)$')
@@ -355,6 +372,18 @@ plt.ylim([0,0.5])
 ax=plt.gca()
 #ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 plt.savefig(folderres+'/ea.'+img,dpi=400)
+
+plt.figure(65)
+plt.plot(radii,Ma,label='simulation')
+plt.plot(radii,Ma_a(radii),label='model')
+plt.xlabel('$a$')
+plt.ylabel('$M_a$')
+plt.xlim([2,13])
+plt.legend()
+ax=plt.gca()
+#ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.savefig(folderres+'/Ma.'+img,dpi=400)
+
 
 #plt.draw()
 #plt.pause(1)

velovert.py
@@ -5,10 +5,11 @@ import sys
 import discEccanalysis_pysplash as de
 import genvecc as gv
 import vertical_structure as vs
+import useful_param as up
 
-img='png'
+img=up.img
 #folderres='./analysis_paper'
-folderres='./sim5A2000'
+folderres=up.folderres
 name=sys.argv[1]
 
 os.system("splash -p nonlog "+name+" -o ascii -r 6 -dev /png")
@@ -140,13 +141,13 @@ plt.title('$H_{\\rm sim}/a$')
 plt.savefig(folderres+'/HRsim.'+img,dpi=400)
 
 plt.figure(4)
-velmax=vz.max()*0.5
+velmax=vz.max()*0.9
 velmin=-velmax
 plt.scatter(xgrplan,ygrplan,c=vzplan,cmap="RdBu_r",vmin=velmin,vmax=velmax)
 plt.colorbar()
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-plt.title('$|v_{z,{\\rm sim}}|$')
+plt.title('$v_{z,{\\rm sim}}$')
 plt.savefig(folderres+'/vzsim.'+img,dpi=400)
 
 plt.figure(5)
@@ -187,7 +188,7 @@ plt.scatter(xgrplan,ygrplan,c=vz/np.abs(vphi),cmap="RdBu_r",vmin=velmin,vmax=vel
 plt.colorbar()
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-plt.title('$|v_{z,{\\rm teor}}|/v_{\\phi}$')
+plt.title('$v_{z,{\\rm teor}}/v_{\\phi}$')
 plt.savefig(folderres+'/vzteor_vphi.'+img,dpi=400)
 
 plt.figure(72)
@@ -197,7 +198,7 @@ plt.scatter(xgrplan,ygrplan,c=(vzplan)/np.abs(vphi),cmap="RdBu_r",vmin=velmin,vm
 plt.colorbar()
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-plt.title('$|v_{z,{\\rm sim}}|/v_{\\phi}$')
+plt.title('$v_{z,{\\rm sim}}/v_{\\phi}$')
 plt.savefig(folderres+'/vzsim_vphi.'+img,dpi=400)
 
 ## A few more plots

vertical_structure.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import eccentric_disc.VerticalStructure as evert
 from scipy.interpolate import RegularGridInterpolator
 import useful_param as up
+import interpolation as itp
 import pdb
 
 a0=up.ain #6. #be careful R_ref not always coincides with R_in
@@ -105,11 +106,11 @@ def vert_struct_solver(H0,e0):
     vz_arr=dh*H0*Omega0 #dharr/htarr*cs(amesh)   
 
     #need now to plot it as eccentric anom
-    interp_H = np.polynomial.Chebyshev.fit(Eanom_arr,H_arr,80)
+    interp_H = itp.interpolator(Eanom_arr,H_arr,80)
     def H_func(EE):
         return interp_H(EE)
 
-    interp_vz =np.polynomial.Chebyshev.fit(Eanom_arr,vz_arr,80)
+    interp_vz =itp.interpolator(Eanom_arr,vz_arr,80)
     def vz_func(EE):
         return interp_vz(EE)
 